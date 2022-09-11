4454cbc9768434a5847bd9fde6544f0c0e8cb755
Dessed <des.ztrcnto@gmail.com>
2022-09-11 21:48:43 +0300

    change 11.09.22

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch ('https://restcountries.com/v3.1/name/{name}?fullText=true');
+fetch ('https://restcountries.com/v3.1/name/aruba?fullText=true');
