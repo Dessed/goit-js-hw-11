ece90240d2e8246663c41c0c3eaadcddd678590e
Dessed <des.ztrcnto@gmail.com>
2022-09-20 18:41:24 +0300

    fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,29 @@
 export {fetchCountries};
+import { renderCountriesData } from './index';
+import { showsListCountries } from './index';
+import Notiflix from 'notiflix';
 
-function fetchCountries(name) {
-    return name = 'Andrey';
+async function fetchCountries(name) {
+   try {
+        const countries = await fetch(`https://restcountries.com/v3.1/name//${name}?fields=name,capital,population,flags,languages`);
+        if (!countries.ok ) {
+            (Notiflix.Notify.failure('Oops, there is no country with that name'));
+        }
+        const countrie = await countries.json();
+        if (!countrie.length) {
+            list.innerHTML = '';
+            infoCountry.innerHTML = '';
+        } else if (countrie.length >= 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (countrie.length === 1) {
+            renderCountriesData(countrie);
+            list.innerHTML = '';
+        } else {
+            showsListCountries(countrie);
+            infoCountry.innerHTML = '';
+        }
+    } catch (error) {
+        console.log('Ошибка');
+      }
 }
+   
--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+export {renderCountriesData};
+export {showsListCountries};
+var debounce = require('lodash.debounce');
+
+const input = document.getElementById('search-box');
+      list = document.querySelector('.country-list');
+      infoCountry = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
-fetch ('https://restcountries.com/v3.1/name/{name}?fullText=true');
-fetch ('https://restcountries.com/v3.1/name/aruba?fullText=true');
+const debounceSearchCountries = debounce(searchNameCountries, DEBOUNCE_DELAY);
+
+input.addEventListener('keydown', debounceSearchCountries);
+
+function searchNameCountries (e) {
+  let name = e.target.value.trim();
+  fetchCountries(name);
+}
+
+function showsListCountries (countries) {
+  list.style.listStyle = 'none';
+  const markup = countries.map(({name, flags}) => {
+    return `<li>
+      <p> <img src="${flags.svg} "width=30px"/>  <b "font-size=40px"> ${name.official}  </b></p>
+      </li>`
+  }).join("");
+  list.innerHTML = markup;
+}
+
+function renderCountriesData(countries) {
+  infoCountry.style.listStyle = 'none';
+
+  const markup = countries.map(({name, capital, population, languages, flags}) => {
+    return `<li style="font-size:40px">
+      <p style="margin-top:10px"> <img src="${flags.svg} "width=30px" style="padding-bottom:4px"/> 
+      <b>${name.official}</b></p></li>
+    
+      <li><p><b>Capital</b>: ${capital}</p></li>
+      <li><p><b>Population</b>: ${population}</p></li>
+      <li><p><b>Languages</b>: ${Object.values(languages).join(', ')}</p></li>`
+  }).join("");
+    infoCountry.innerHTML = markup;
+}
